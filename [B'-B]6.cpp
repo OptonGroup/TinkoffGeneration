5287ced8fde3e2ce667230f7f843ee766952b242
Opton <52974908+OptonGroup@users.noreply.github.com>
2022-09-03 20:36:41 +0400

    Add files via upload

--- [B'-B]6.cpp
+++ [B'-B]6.cpp
@@ -10,36 +10,25 @@ int INF = 2 << 18;
 ll n, m;
 ll res = 1;
 vector<vector<int>> g(110000);
-vector <int> used, cnt(110000), por, use;
-int ver;
+vector <int> used;
 
-void dfs(int v, int dist){
-	used[v] = ver+10;
-	if (dist > 2 && !use[v]) cnt[v]++;
-	for (auto &u: g[v]){
-		if (used[u] != ver+10) dfs(u, dist+1);
-	}
-	return;
-}
-
-// 485525898
-
-void dfs_get_v(int v){
+void dfs(int v, pair<int, int> cnt){
+	res *= max(0LL, m - cnt.first - cnt.second);
+	res %= 1000000007;
+	
 	used[v] = 1;
-	por.push_back(v);
+	cnt.second = cnt.first;
+	cnt.first = 1;
 	for (auto &u: g[v]){
-		if (!used[u])
-			dfs_get_v(u);
+		if (!used[u]){
+			dfs(u, cnt);
+			cnt.second++;
+		}
 	}
 	return;
 }
 
 int main() {
-// #ifndef ONLINE_JUDGE
-// 	freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
-// #endif // !ONLINE_JUDGE
-// 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
 	cin >> n >> m;
 	for (int i = 0; i < n-1; ++i){
 		int f, t;
@@ -49,24 +38,9 @@ int main() {
 	}
 
 	used = vector<int> (n+1, 0);
-	for (int i = 1; i <= n; ++i){
-		if (g[i].size() == 1){
-			dfs_get_v(i);
-			break;
-		}
-	}
-
-	use = vector<int> (n+1, 0);
-	ll res = 1;
-	if (n == 1) res = m;
-	for (int i = 0; i < por.size(); ++i){
-		ver = por[i];
-		use[ver] = 1;
-		dfs(ver, 0);
-		res *= max(0LL, m-i+cnt[ver]);
-		res %= 1000000007;
-	}
-	cout << res;
+	
+	dfs(1, {0, 0});
+	cout << res;	
 
 	cerr << "Time: " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
 	return 0;
