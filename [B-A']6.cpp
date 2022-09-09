d69225ffa1d480174b833de7d3dc803284a395f2
Opton <52974908+OptonGroup@users.noreply.github.com>
2022-09-09 18:19:31 +0400

    Update [B-A']6.cpp

--- [B-A']6.cpp
+++ [B-A']6.cpp
@@ -12,45 +12,45 @@
 typedef long long ll;
 using namespace std;
 
-struct polindrom_tree {
-	const int maxn = 10, k = 26;
+const int maxn = 1e6+1000, k = 26;
 
-	vector<int> s, len, link;
-	vector<vector<int>> to;
+int str[maxn], len[maxn], link[maxn], to[maxn][k];
 
-	int n, last, sz;
+int n, last, sz;
 
-	polindrom_tree() {
-		n = 0;
-		last = 1;
-		s[n++] = -1;
-		link[0] = 1;
-		len[1] = -1;
-		sz = 2;
+void init() {
+	fill(link, link + maxn, 0);
+	fill(str, str + maxn, 0);
+	fill(len, len + maxn, 0);
+	n = 0;
+	last = 1;
+	str[n++] = -1;
+	link[0] = 1;
+	len[1] = -1;
+	sz = 2;
+}
+
+int get_link(int v) {
+	while (str[n - len[v] - 2] != str[n-1])
+		v = link[v];
+	return v;
+}
+
+void add_letter(int c) {
+	c -= 'a';
+	str[n++] = c;
+	last = get_link(last);
+	if (!to[last][c]) {
+		len[sz] = len[last] + 2;
+		link[sz] = to[get_link(link[last])][c];
+		to[last][c] = sz++;
 	}
+	last = to[last][c];
+}
 
-	int get_link(int v) {
-		while (s[n - len[v] - 2] != s[n - 1])
-			v = link[v];
-		return v;
-	}
-
-	void add_letter(int c) {
-		c -= 'a';
-		s[n++] = c;
-		last = get_link(last);
-		if (!to[last][c]) {
-			len[sz] = len[last] + 2;
-			link[sz] = to[get_link(link[last])][c];
-			to[last][c] = sz++;
-		}
-		last = to[last][c];
-	}
-
-	int max_letter() {
-		return len[last];
-	}
-};
+int max_letter() {
+	return len[last];
+}
 
 void solve() {
 	string s;
@@ -68,12 +68,12 @@ void solve() {
 	// del middle left
 	string a = "";
 	for (int i = ind; i < len - ind; ++i) a += s[i];
-	polindrom_tree pt1;
+	init();
 	for (auto u : a)
-		pt1.add_letter(u);
-	if (max_s.size() < ns.size() * 2 + pt1.max_letter()) {
+		add_letter(u);
+	if (max_s.size() < ns.size() * 2 + max_letter()) {
 		max_s = ns;
-		for (int i = 1; i <= pt1.max_letter(); ++i) {
+		for (int i = 1; i <= max_letter(); ++i) {
 			max_s += a[a.size() - i];
 		}
 		reverse(all(ns));
@@ -83,12 +83,12 @@ void solve() {
 
 	// del middle right
 	reverse(all(a));
-	polindrom_tree pt2;
+	init();
 	for (auto u : a)
-		pt2.add_letter(u);
-	if (max_s.size() < ns.size() * 2 + pt2.max_letter()) {
+		add_letter(u);
+	if (max_s.size() < ns.size() * 2 + max_letter()) {
 		max_s = ns;
-		for (int i = 1; i <= pt2.max_letter(); ++i) {
+		for (int i = 1; i <= max_letter(); ++i) {
 			max_s += a[a.size() - i];
 		}
 		reverse(all(ns));
@@ -97,24 +97,24 @@ void solve() {
 	}
 
 	// del left
-	polindrom_tree pt3;
+	init();
 	for (auto u : s)
-		pt3.add_letter(u);
-	if (max_s.size() < pt3.max_letter()) {
+		add_letter(u);
+	if (max_s.size() < max_letter()) {
 		max_s = "";
-		for (int i = 1; i <= pt3.max_letter(); ++i) {
+		for (int i = 1; i <= max_letter(); ++i) {
 			max_s += s[s.size() - i];
 		}
 	}
 
 	// del right
 	reverse(all(s));
-	polindrom_tree pt4;
+	init();
 	for (auto u : s)
-		pt4.add_letter(u);
-	if (max_s.size() < pt4.max_letter()) {
+		add_letter(u);
+	if (max_s.size() < max_letter()) {
 		max_s = "";
-		for (int i = 1; i <= pt4.max_letter(); ++i) {
+		for (int i = 1; i <= max_letter(); ++i) {
 			max_s += s[s.size() - i];
 		}
 	}
